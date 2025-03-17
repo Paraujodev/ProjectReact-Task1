042ed057ff4853bbfa3466e8ffe3f937bd3bbdc7
unknown <paalbertosilva@gmail.com>
2025-03-17 10:16:45 -0300

    Reiniciado o projeto do zero devido a erros de compilacao

---
+++ efont-project/src/components/navbar.jsx
@@ -0,0 +1,17 @@
+
+
+export default function Navbar(){
+    return (
+        <nav>
+           <a> logo do site </a> 
+           <ul>
+            <li>Dashboard</li>
+            <li>Carteira</li>
+            <li>Transações</li>
+            <li>Suporte</li>
+            <li>Ajustes</li>
+            <li>Desconectar</li>
+           </ul>
+        </nav>
+    )
+}
