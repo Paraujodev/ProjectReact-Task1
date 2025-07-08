593ae0d27c39a00af4b6c1cbb631fdaaf61e6ab3
unknown <paalbertosilva@gmail.com>
2025-07-08 00:50:35 -0300

    feat-adicionado as funcionalidades para buscar da api

--- efront-project/src/components/mainTable/mainTable.jsx
+++ efront-project/src/components/mainTable/mainTable.jsx
@@ -1,72 +1,79 @@
-import './style-mainTable.scss'
-import { useEffect, useRef, useState } from 'react';
-
-
+import './style-mainTable.scss';
+import { useEffect, useState } from 'react';
+import Loading from '../../assets/loading.gif';
 
 export default function MainTable() {
+    const [users, setUsers] = useState([]);
+    const [loadingTable, setLoadingTable] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch("https://my.api.mockaroo.com/historico_compradores.json", {
             headers: {
-                "X-API-Key": "undefined"
+                "X-API-Key": "420e6730"
             }
-        })//fechamento do fetch
-
+        })
             .then((response) => {
                 if (!response.ok) {
-                    throw new Error("Erro na requisição");
+                    throw new Error("Erro ao buscar dados");
                 }
                 return response.json();
-            })//fechamento do then
-
-            .then((data) => {
-                setDados(data);
-                setLoading(false);
-            })//fechamento do then
-
+            })
+            .then((dataTable) => {
+                setUsers(dataTable);
+                setLoadingTable(false);
+                //console.log(dataTable);
+            })
             .catch((error) => {
                 console.error("Erro ao buscar dados:", error);
-                setLoading(true);
-            });//fechamento do catch
+                setError(error.message);
+                setLoadingTable(false);
+            });
     }, []);
-
-
 
     return (
         <div className='boxMainTable'>
             <h2>Histórico de compradores</h2>
-            <div className="table-responsive">
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">ID</th>
-                            <th scope="col">Data</th>
-                            <th scope="col">Nome</th>
-                            <th scope="col">Localização</th>
-                            <th scope="col">Valor</th>
-                            <th scope="col">Situação</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>#123456</td>
-                            <td>02/08/2023</td>
-                            <td>Roberto Santos</td>
-                            <td>Rio Grande do Sul</td>
-                            <td>R$ 15,00</td>
-                            <td>Aprovado</td>
-                        </tr>
-                        <tr>
-                            <td>#67890</td>
-                            <td>03/08/2023</td>
-                            <td>Beatriz Rodrigues</td>
-                            <td>Salvador</td>
-                            <td>R$ 15,00</td>
-                            <td>Em análise</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+
+            {loadingTable && <img src={Loading} alt="Carregando..." />}
+            {error && <p style={{ color: 'red' }}>Erro: {error}</p>}
+
+            {!loadingTable && !error && (
+                <div className="table-responsive">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Data</th>
+                                <th>Nome</th>
+                                <th>Localização</th>
+                                <th>Valor</th>
+                                <th>Situação</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {users.map((usuario) => (
+                                <tr key={usuario.id}>
+                                    <td>{usuario.id}</td>
+                                    <td>{usuario.date}</td>
+                                    <td>{usuario.name}</td>
+                                    <td>{usuario.state}</td>
+                                    <td>{usuario.value}</td>
+                                    <td>{usuario.status}</td>
+                                </tr>
+                            ))}
+                            <tr>
+                                <td>#67890</td>
+                                <td>03/08/2023</td>
+                                <td>Beatriz Rodrigues</td>
+                                <td>Salvador</td>
+                                <td>R$ 15,00</td>
+                                <td>Em análise</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            )}
         </div>
-    )
+    );
 }
