d805794956950ac8dc68b3428cb639058da3ec7f
VRocca10 <viniciusrocca5@gmail.com>
2025-07-29 12:43:04 -0300

    reafcture(register): modifying the background

--- efront-project/src/pages/register/register.jsx
+++ efront-project/src/pages/register/register.jsx
@@ -16,7 +16,7 @@ function Register() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-
+        
         if (password !== confirmPassword) {
             setPasswordError("As senhas não coincidem.");
             return;
@@ -36,92 +36,94 @@ function Register() {
     };
 
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <header>
-                        <h1 className="title">Crie sua Conta</h1>
-                        <h2 className="subtitle">Preencha seus dados</h2>
-                    </header>
+        <div className="image-background">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <header>
+                            <h1 className="title">Crie sua Conta</h1>
+                            <h2 className="subtitle">Preencha seus dados</h2>
+                        </header>
 
-                    <form onSubmit={handleSubmit}>
-                        <label>
-                            <span>Nome completo *</span>
-                            <input
-                                type="text"
-                                value={fullName}
-                                onChange={(e) => setFullName(e.target.value)}
-                                required
-                            />
-                        </label>
-
-                        <label>
-                            <span>Profissão</span>
-                            <input
-                                type="text"
-                                value={profession}
-                                onChange={(e) => setProfession(e.target.value)}
-                            />
-                        </label>
-
-                        <label>
-                            <span>E-mail *</span>
-                            <input
-                                type="email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                required
-                            />
-                        </label>
-
-                        <label>
-                            <span>Senha *</span>
-                            <div className="password-wrapper">
+                        <form onSubmit={handleSubmit}>
+                            <label>
+                                <span>Nome completo *</span>
                                 <input
-                                    type={showPassword ? "text" : "password"}
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
+                                    type="text"
+                                    value={fullName}
+                                    onChange={(e) => setFullName(e.target.value)}
                                     required
                                 />
-                                <i
-                                    className={`fa-solid ${showPassword ? "fa-eye-slash" : "fa-eye"} toggle-password-icon`}
-                                    onClick={() => setShowPassword(!showPassword)}
-                                ></i>
-                            </div>
-                        </label>
+                            </label>
 
-                        <label>
-                            <span>Confirmação de senha *</span>
-                            <div className="password-wrapper">
+                            <label>
+                                <span>Profissão</span>
                                 <input
-                                    type={showConfirmPassword ? "text" : "password"}
-                                    value={confirmPassword}
-                                    onChange={(e) => setConfirmPassword(e.target.value)}
+                                    type="text"
+                                    value={profession}
+                                    onChange={(e) => setProfession(e.target.value)}
+                                />
+                            </label>
+
+                            <label>
+                                <span>E-mail *</span>
+                                <input
+                                    type="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
                                     required
                                 />
-                                <i
-                                    className={`fa-solid ${showConfirmPassword ? "fa-eye-slash" : "fa-eye"} toggle-password-icon`}
-                                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                                ></i>
-                            </div>
-                        </label>
-                        {passwordError && (
-                            <p className="error-message">{passwordError}</p>
-                        )}
+                            </label>
 
-                        <label className="checkbox-label">
-                            <input
-                                type="checkbox"
-                                checked={agreeTerms}
-                                onChange={(e) => setAgreeTerms(e.target.checked)}
-                                required
-                            />
-                            Eu concordo com os <a>Termos de Política de Privacidade</a> *
-                        </label>
-                        <button className="button-cadastro" type="submit">Cadastrar</button>
-                        <button className="button-login" type="submit">Já tem uma conta? <b>Faça Login.</b>
-                        </button>
-                    </form>
+                            <label>
+                                <span>Senha *</span>
+                                <div className="password-wrapper">
+                                    <input
+                                        type={showPassword ? "text" : "password"}
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        required
+                                    />
+                                    <i
+                                        className={`fa-solid ${showPassword ? "fa-eye-slash" : "fa-eye"} toggle-password-icon`}
+                                        onClick={() => setShowPassword(!showPassword)}
+                                    ></i>
+                                </div>
+                            </label>
+
+                            <label>
+                                <span>Confirmação de senha *</span>
+                                <div className="password-wrapper">
+                                    <input
+                                        type={showConfirmPassword ? "text" : "password"}
+                                        value={confirmPassword}
+                                        onChange={(e) => setConfirmPassword(e.target.value)}
+                                        required
+                                    />
+                                    <i
+                                        className={`fa-solid ${showConfirmPassword ? "fa-eye-slash" : "fa-eye"} toggle-password-icon`}
+                                        onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                                    ></i>
+                                </div>
+                            </label>
+                            {passwordError && (
+                                <p className="error-message">{passwordError}</p>
+                            )}
+
+                            <label className="checkbox-label">
+                                <input
+                                    type="checkbox"
+                                    checked={agreeTerms}
+                                    onChange={(e) => setAgreeTerms(e.target.checked)}
+                                    required
+                                />
+                                Eu concordo com os <a>Termos de Política de Privacidade</a> *
+                            </label>
+                            <button className="button-cadastro" type="submit">Cadastrar</button>
+                            <button className="button-login" type="submit">Já tem uma conta? <b>Faça Login.</b>
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
